fdb16159089225b22ff543fb69d4268fa5ff7e92
Fredy Aristizabal <freddyarh13@gmail.com>
2023-04-13 18:09:53 +1000

    change api url to journal

--- server/server.ts
+++ server/server.ts
@@ -14,7 +14,7 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        this.usersPath = '/api'; 
+        this.usersPath = '/journal'; 
 
         this.connectDB();
         this.middlewares(); 
